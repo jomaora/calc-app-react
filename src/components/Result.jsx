0072c0674c8ef4d3d32578b34a418796c7f07150
Joan Ortega <jomaora@gmail.com>
2022-07-17 11:42:03 +0200

    proptypes and defaultTypes

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const Result = ({value}) => {
   return (<div className="result">
     <span>{value}</span>
   </div>);
+};
+
+Result.propTypes = {
+  value: PropTypes.number.isRequired
+};
+
+Result.defaultProps = {
+  value: 0
 };
 
 export default Result;
