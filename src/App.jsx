c2ec6b9318e78d54f02e23d73dc050df5efc68d9
Joan Ortega <jomaora@gmail.com>
2022-08-10 09:12:33 +0200

    Adding eval to compute the operation

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { number } from "prop-types";
 import React, {useState} from "react";
 import "./App.css";
 import Functions from "./components/Functions";
@@ -6,16 +5,29 @@ import MathOperations from "./components/MathOperations";
 import Numbers from "./components/Numbers";
 import Result from "./components/Result";
 
+const REGEX = /[^-^+^*^/]+/g;
+
 const App = () => {
   const [stack, setStack] = useState('');
 
   const clickHandler = number => {
-    console.log('clickHandler', number, stack);
-    setStack(stack + number)
+    setStack(`${stack}${number}`)
   };
 
   const onContentClear = () => {
     setStack('');   
+  }
+
+  const onDelete = () => {
+    if (stack === '') return;
+    const newString = stack.substring(0, stack.length - 1);
+    setStack(newString);
+  }
+
+  const onEqual = () => {
+    // eslint-disable-next-line no-eval
+    const result = eval(stack);
+    setStack(result.toString());
   }
 
   console.log('Renderizacion de App');
@@ -24,8 +36,12 @@ const App = () => {
       Calc App
       <Result value={stack} />
       <Numbers onClickNumber={clickHandler} />
-      <Functions onContentClear={onContentClear} onDelete={() => console.log('R')}/>
-      <MathOperations onClickOperation={clickHandler} onClickEqual={equal => console.log(equal)} />
+      <Functions 
+        onContentClear={onContentClear} 
+        onDelete={onDelete}/>
+      <MathOperations 
+        onClickOperation={clickHandler} 
+        onClickEqual={onEqual} />
     </main>)
 };
 
